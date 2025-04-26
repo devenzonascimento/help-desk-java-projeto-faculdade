266575a1d4eef9bad68ab967e486a5bc2b681202
Enzo Nascimento <143226080+devenzonascimento@users.noreply.github.com>
2025-04-26 08:50:54 -0300

    feat: Adiciona Controller de Categoria.

---
+++ src/main/java/project/Controllers/CategoryController.java
@@ -0,0 +1,23 @@
+package project.Controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import project.DTOS.CategoryDTO;
+import project.Entities.Category;
+import project.Services.CategoryService;
+
+@RestController
+@RequestMapping("/category")
+public class CategoryController {
+
+    @Autowired
+    CategoryService categoryService;
+
+    @PostMapping("/{category}")
+    public @ResponseBody ResponseEntity<CategoryDTO> create(@PathVariable String category) {
+        Category createdCategory = categoryService.create(category);
+
+        return ResponseEntity.ok().body(CategoryDTO.valueOf(createdCategory));
+    }
+}
