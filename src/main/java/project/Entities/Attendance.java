ecd514cb9a028ca8a21d7705bb456c9c642879ed
Enzo Nascimento <143226080+devenzonascimento@users.noreply.github.com>
2025-05-21 03:27:36 -0300

    feat: Adiciona a entidade Attendance.

---
+++ src/main/java/project/Entities/Attendance.java
@@ -0,0 +1,54 @@
+package project.Entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.JdbcType;
+import org.hibernate.dialect.PostgreSQLEnumJdbcType;
+import project.Enums.AttendanceStatus;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "attendance", schema = "public")
+public class Attendance implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 139812389168912389L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @JdbcType(PostgreSQLEnumJdbcType.class)
+    @Column(
+        name = "status",
+        nullable = false,
+        columnDefinition = "AttendanceStatus"
+    )
+    private AttendanceStatus status;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_team_id", nullable = false)
+    private UserTeam userTeam;
+
+    @Column(name = "date", nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private LocalDateTime date;
+
+    @Column(name = "description", nullable = false, columnDefinition = "TEXT", length = 1000)
+    private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "ticket_id", referencedColumnName = "id", nullable = false)
+    private Ticket ticket;
+
+    // @Column(name = "file_id")
+    // private Long fileId;
+}
---
+++ src/main/java/project/Enums/AttendanceStatus.java
@@ -0,0 +1,9 @@
+package project.Enums;
+
+public enum AttendanceStatus {
+    AWAITING,     // Aguardando
+    STARTED,      // Iniciado
+    FORWARDED,    // Encaminhado
+    RETURNED,     // Devolvido
+    COMPLETED     // Concluído
+}
