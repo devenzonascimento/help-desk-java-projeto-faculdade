8cf5c8214f97f5e962348f84fc2f07632d76a09d
Enzo Nascimento <143226080+devenzonascimento@users.noreply.github.com>
2025-05-21 03:27:45 -0300

    feat: Adiciona a entidade Ticket.

---
+++ src/main/java/project/Entities/Ticket.java
@@ -0,0 +1,69 @@
+package project.Entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.JdbcType;
+import org.hibernate.dialect.PostgreSQLEnumJdbcType;
+import project.Enums.AttendanceStatus;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "ticket", schema = "public")
+public class Ticket implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 139812389168912389L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "requester", nullable = false)
+    private User requester;
+
+    @Enumerated(EnumType.STRING)
+    @JdbcType(PostgreSQLEnumJdbcType.class)
+    @Column(
+        name = "last_status",
+        nullable = false,
+        columnDefinition = "AttendanceStatus"
+    )
+    private AttendanceStatus lastStatus;
+
+    @Column(name = "start_date", nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private LocalDateTime startDate;
+
+    @Column(name = "end_date")
+    @Temporal(TemporalType.TIMESTAMP)
+    private LocalDateTime endDate;
+
+    @Column(name = "title", nullable = false, length = 50)
+    private String title;
+
+    @Column(name = "description", nullable = false, columnDefinition = "TEXT", length = 1000)
+    private String description;
+
+    @Column(name = "internal_report", columnDefinition = "TEXT", length = 1000)
+    private String internalReport;
+
+    @OneToMany(cascade =  CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinColumn(name = "ticket_id")
+    private List<Attendance> attendances;
+
+    @ManyToOne
+    @JoinColumn(name = "team_id", referencedColumnName = "id", nullable = false)
+    private Team team;
+
+    // @Column(name = "file_id")
+    // private Long fileId;
+}
